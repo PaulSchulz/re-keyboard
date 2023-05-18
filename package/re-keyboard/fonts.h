6d99060f135c1c37439f43f0e454be3ec006cd8c
Paul Schulz <paul@mawsonlakes.org>
2023-05-18 19:52:24 +0930

    Adds font files

---
+++ package/re-keyboard/fonts.h
@@ -0,0 +1,23 @@
+#ifndef _CCOW_H_
+#define _CCOW_H_
+
+#include <stdint.h>
+
+#define MOD_CAPS  0x0001
+#define MOD_SHIFT 0x0002
+#define MOD_CTRL  0x0004
+#define MOD_ALT   0x0008
+
+#define limit_left    1800
+#define limit_right  14500
+#define limit_top    19000
+#define limit_bottom  1200
+
+const int8_t* get_font_char(const char* font_name,
+                            char ascii_value,
+                            int* out_num_verts,
+                            int* out_horiz_dist);
+
+char keycode_to_ascii(int keycode, int mods);
+
+#endif // _CCOW_H_
